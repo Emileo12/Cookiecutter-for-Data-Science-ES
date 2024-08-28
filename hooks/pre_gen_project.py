5834cabc07ab27ede6bfa23ae9ccd17748db5d75
LeonardoV <carlosvannoni21@gmail.com>
2024-08-27 22:08:25 -0400

    Se arreglo el pre_gen y el post_gen: funcionalidad de crear ambiente virtual

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import subprocess
+
 
 project_slug = "{{ cookiecutter.project_slug }}"
 python_version = "{{ cookiecutter.python_version }}"
@@ -16,25 +16,3 @@ if project_slug.startswith("x"):
 print(f"{MESSAGE_COLOR}Let\'s do it! You\'re going to create something awesome!")
 print(f"Creating project at {os.getcwd()}{RESET_ALL}")
 
-# Función para crear el ambiente virtual con Conda
-def create_conda_env():
-    print(f"{MESSAGE_COLOR}Creando ambiente virtual con Conda...{RESET_ALL}")
-    env_file = 'environment.yml'
-    
-    # Verificar si el archivo environment.yml existe
-    if not os.path.exists(env_file):
-        print(f"{ERROR_COLOR}ERROR: {env_file} no encontrado.{RESET_ALL}")
-        sys.exit(1)
-    
-    # Comando para crear el ambiente con Conda
-    command = f'conda env create --file {env_file}'
-    
-    try:
-        subprocess.check_call(command, shell=True)
-        print(f"{MESSAGE_COLOR}Ambiente Conda creado con éxito.{RESET_ALL}")
-    except subprocess.CalledProcessError as e:
-        print(f"{ERROR_COLOR}ERROR: Falló la creación del ambiente Conda.{RESET_ALL}")
-        sys.exit(1)
-
-# Llama a la función para crear el ambiente Conda
-create_conda_env()
